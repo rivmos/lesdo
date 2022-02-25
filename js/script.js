8e40011365a6edd4b621ae9ac318dea003794a1f
rivmos <rivmos.official@gmail.com>
2022-02-25 17:55:46 +0530

    added title but with separate array for title

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', addNotes);
 
 function addNotes() {
-    if (localStorage.notes) {
+    if (localStorage.notes && localStorage.titles) {
         notesObj = JSON.parse(localStorage.getItem('notes'));
+        titlesObj = JSON.parse(localStorage.getItem('titles'));
     }
     else {
         notesObj = [];
+        titlesObj = [];
     }
     // // We Can Also Use :
     // if(localStorage.notes == null){
@@ -18,11 +20,14 @@ function addNotes() {
     // else{
     //     notesObj = JSON.parse(localStorage.getItem('notes'));
     // }
-
+    let noteTitle = document.getElementById('noteTitle');
     let textToAdd = document.getElementById('textToAdd');
     notesObj.push(textToAdd.value);
+    titlesObj.push(noteTitle.value);
     localStorage.setItem('notes', JSON.stringify(notesObj));
+    localStorage.setItem('titles', JSON.stringify(titlesObj));
     textToAdd.value = "";
+    noteTitle.value = "";
     showNotes();
 }
 
@@ -30,22 +35,25 @@ function showNotes() {
 
     let html = '';
     let notes = localStorage.getItem('notes');
-    if(notes == null){
+    let titles = localStorage.getItem('titles');
+    if(notes == null && titles == null){
         notesObj = [];
+        titlesObj = [];
     }
     else{
         notesObj = JSON.parse(notes);
+        titlesObj = JSON.parse(titles);
     }
     notesObj.forEach(function (element, index) {
         html += `
                 <div class="note">
-                    <h4>Note ${index + 1}</h4>
+                    <h4>${titlesObj[index]}</h4>
                     <p>${element}</p>
-                    <button id="${index + 1}" onclick="delNotes(this.id)" class="delBtn">Delete Note</button>
+                    <button id="${index}" onclick="delNotes(this.id)" class="delBtn">Delete Note</button>
                 </div>`;
     })
     let allNotes = document.querySelector('.allNotes');
-    if(notesObj.length == 0){
+    if(notesObj.length == 0 && titlesObj.length == 0){
         allNotes.innerHTML = `Nothing to show! Use "Add a Note" section to add notes.`;
     }
     else {
@@ -57,9 +65,13 @@ function showNotes() {
 
 function delNotes(index){
     let notes = localStorage.getItem('notes');
+    let titles = localStorage.getItem('titles');
     notesObj = JSON.parse(notes);
-    notesObj.splice(index-1,1);
+    titlesObj = JSON.parse(titles);
+    notesObj.splice(index,1);
+    titlesObj.splice(index,1);
     localStorage.setItem('notes', JSON.stringify(notesObj));
+    localStorage.setItem('titles',JSON.stringify(titlesObj));
     showNotes();
 }
 
@@ -79,9 +91,11 @@ function searchNotes(){
     let searchText =  searchInput.value.toLowerCase();
     let notes = document.getElementsByClassName('note');
     Array.from(notes).forEach(function(element){
+        let title = element.querySelector('h4');
         let note = element.querySelector('p');
         note = note.innerText;
-        if(note.toLowerCase().includes(searchText)){
+        title = title.innerText;
+        if(note.toLowerCase().includes(searchText) || title.toLowerCase().includes(searchText)){
             element.style.display = 'block';
         }
         else{
